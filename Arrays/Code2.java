8fb460674eee22a07beef6e07a1fc29d6023d7db
SparshSingh700 <148028935+SparshSingh700@users.noreply.github.com>
2025-05-26 22:15:27 +0530

    Move All Zeros to the End

---
+++ Arrays/Code2.java
@@ -0,0 +1,16 @@
+//Day2 - Move All Zeros to End 
+class Solution {
+    void pushZerosToEnd(int[] arr) {
+        int i=0;
+        int j=i;
+        while(j<arr.length){
+            if(arr[j]!=0){
+                int temp = arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
+                i++;
+            }
+        j++;
+        }
+    }
+}
