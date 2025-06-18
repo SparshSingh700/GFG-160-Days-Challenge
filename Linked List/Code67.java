652c948fb4db1a3210bf865678613294ce7d1414
SparshSingh700 <148028935+SparshSingh700@users.noreply.github.com>
2025-06-18 21:57:58 +0530

    Merge two sorted linked lists

---
+++ Linked List/Code67.java
@@ -0,0 +1,36 @@
+//Gfg Merge two sorted linked lists
+/*
+  Node is defined as
+    class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+*/
+
+class Solution {
+    Node sortedMerge(Node head1, Node head2) {
+        // code here
+        Node temp1=head1;
+        Node temp2=head2;
+        Node dummy=new Node(-1);
+        Node res=dummy;
+        while(temp1 !=null & temp2 !=null){
+            if(temp1.data<temp2.data){
+                res.next=temp1;
+                temp1=temp1.next;
+            }
+            else{
+                res.next=temp2;
+                temp2=temp2.next;
+            } 
+            res=res.next;
+        }
+        if(temp1!=null){
+            res.next=temp1;
+        }
+        else res.next=temp2;
+        return dummy.next;
+    }
+}
